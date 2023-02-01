25bf17caf9c3fa7a11f45689cdeaa02eb9a67436
Werner Rall <weral@microsoft.com>
2023-02-01 18:43:03 +0200

    Updated Deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,11 +1,13 @@
 Connect-AzAccount
 
-$resourceGroup = Read-Host "Please supply a name for the Resource Group:"
-
+$resourceGroup = Read-Host "Please supply a name for the Resource Group:  "
+$genServ = Read-Host "Please Enter a Random Number between between 10000 and 20000:  "
+$storac = "sbsastoragedemo$genServ"
 
 Write-Host "Generating template parameters..." -ForegroundColor Green
 $TemplateParameters = @{
-    'resourceGroupName' = $resourceGroup
+   	'resourceGroupName' = $resourceGroup
+	'storageAccountName' = $storac
  }
 
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ".\azuredeploy.json" -templateparameterobject $TemplateParameters -ErrorAction Stop

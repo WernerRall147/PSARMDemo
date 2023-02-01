f0141abc0fd23163afc222b56d80b650bd4f0916
Werner Rall <weral@microsoft.com>
2023-02-01 18:37:58 +0200

    Updated Deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,11 @@
+Connect-AzAccount
+
 $resourceGroup = Read-Host "Please supply a name for the Resource Group:"
+
+
+Write-Host "Generating template parameters..." -ForegroundColor Green
+$TemplateParameters = @{
+    'resourceGroupName' = $resourceGroup
+ }
+
 New-AzResourceGroupDeployment -ResourceGroupName $resourceGroup -TemplateFile ".\azuredeploy.json" -templateparameterobject $TemplateParameters -ErrorAction Stop
